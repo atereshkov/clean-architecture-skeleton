6d3ad0a86bad82b3586d29961aa0acc202ec099b
Alexander Tereshkov <alexander@tereshkov.pw>
2021-08-08 08:42:19 +0300

    Initial project commit with pods

---
+++ clean-architecture-skeleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clean-architecture-skeleton
+//
+//  Created by nb-058-41b on 8/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
